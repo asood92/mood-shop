492e963dda2ecf058baec001791498fbbe62deae
Amman Sood <392075+asood92@users.noreply.github.com>
2020-10-07 17:20:27 -0400

    fix indentation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -38,53 +38,52 @@ data.forEach(function (element, index) {
     newDiv.appendChild(button);
     // put new div inside items container
     itemsContainer.appendChild(newDiv);
-
-    // create array for all items
-    const cart = [];
-
-    function addItem(name, price) {
-        for (let i = 0; i < cart.length; i += 1) {
-            if (cart[i].name === name) {
-                cart[i].qty += 1;
-                return;
-            }
-        }
-        const item = { name: name, price: price, qty: 1 };
-        cart.push(item);
-    }
-
-    // show items
-    function showItems() {
-        const qty = getQty();
-        console.log(`You have ${qty} items in your cart`);
-        for (let i = 0; i < cart.length; i += 1) {
-            console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty} `);
-        }
-        console.log(`Total in cart: $${getTotal()}`);
-    }
-
-    // get quantity
-    function getQty() {
-        let qty = 0;
-        for (let i = 0; i < cart.length; i += 1) {
-            qty += cart[i].qty;
-        }
-        return qty;
-    }
-
-    // get total
-    function getTotal() {
-        let total = 0;
-        for (let i = 0; i < cart.length; i += 1) {
-            total += cart[i].price * cart[i].qty;
-        }
-        return total.toFixed(2);
-    }
-
-    addItem("Apple", 0.99);
-    addItem("Orange", 1.29);
-    addItem("Opinion", 0.02);
-    addItem("Apple", 0.99);
-
-    showItems();
 });
+
+// create array for all items
+const cart = [];
+function addItem(name, price) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1;
+            return;
+        }
+    }
+    const item = { name: name, price: price, qty: 1 };
+    cart.push(item);
+}
+
+// show items
+function showItems() {
+    const qty = getQty();
+    console.log(`You have ${qty} items in your cart`);
+    for (let i = 0; i < cart.length; i += 1) {
+        console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty} `);
+    }
+    console.log(`Total in cart: $${getTotal()}`);
+}
+
+// get quantity
+function getQty() {
+    let qty = 0;
+    for (let i = 0; i < cart.length; i += 1) {
+        qty += cart[i].qty;
+    }
+    return qty;
+}
+
+// get total
+function getTotal() {
+    let total = 0;
+    for (let i = 0; i < cart.length; i += 1) {
+        total += cart[i].price * cart[i].qty;
+    }
+    return total.toFixed(2);
+}
+
+addItem("Apple", 0.99);
+addItem("Orange", 1.29);
+addItem("Opinion", 0.02);
+addItem("Apple", 0.99);
+
+showItems();
