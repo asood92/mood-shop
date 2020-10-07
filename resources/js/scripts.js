e1760394ee5e4ecec5077f8a2ba3215a6fb3b0ab
Amman Sood <392075+asood92@users.noreply.github.com>
2020-10-07 19:41:52 -0400

    cart fully functional

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,6 @@
 import data from "./data.js";
 const itemsContainer = document.getElementById("items");
 const itemList = document.getElementById("item-list");
-itemList.innerHTML = "<li> Hello World </li>";
 const cartQty = document.getElementById("cart-qty");
 const cartTotal = document.getElementById("cart-total");
 
@@ -47,16 +46,66 @@ data.forEach(function (element, index) {
 // create array for all items
 const cart = [];
 
+itemList.onclick = function (e) {
+    if (e.target && e.target.classList.contains("remove")) {
+        const name = e.target.dataset.name; // data-name
+        removeItem(name);
+    } else if (e.target && e.target.classList.contains("add-1")) {
+        const name = e.target.dataset.name;
+        addItem(name);
+    } else if (e.target && e.target.classList.contains("minus-1")) {
+        const name = e.target.dataset.name;
+        removeItem(name, 1);
+    }
+};
+
+itemList.onchange = function (e) {
+    if (e.target && e.target.classList.contains("update")) {
+        const name = e.target.dataset.name;
+        const qty = parseInt(e.target.value);
+        updateCart(name, qty);
+    }
+};
+
+function updateCart(name, qty) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name);
+                return;
+            }
+            cart[i].qty = qty;
+            showItems();
+            return;
+        }
+    }
+}
 // function to add all items to a
 function addItem(name, price) {
     for (let i = 0; i < cart.length; i += 1) {
         if (cart[i].name === name) {
             cart[i].qty += 1;
-            return qty;
+            showItems();
+            return;
         }
     }
     const item = { name: name, price: price, qty: 1 };
     cart.push(item);
+}
+
+function removeItem(name, qty = 0) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty > 0) {
+                cart[i].qty -= qty;
+            }
+            if (cart[i].qty < 1 || qty === 0) {
+                cart.splice(i, 1);
+            }
+            showItems();
+            return;
+        }
+    }
 }
 
 // show items
@@ -68,7 +117,12 @@ function showItems() {
     for (let i = 0; i < cart.length; i += 1) {
         const { name, price, qty } = cart[i];
 
-        itemStr += `<li>${name} $${price} x ${qty} = $${qty * price}</li>`;
+        itemStr += `<li>${name} $${price} x ${qty} = $${qty * price}
+        <button class="remove" data-name="${name}">Remove</button>
+        <button class="add-1" data-name="${name}"> + </button>
+        <button class="minus-1" data-name="${name}"> - </button>
+        <input class="update" type="number" data-name="${name}">
+        </li>`;
     }
     itemList.innerHTML = itemStr;
     cartTotal.innerHTML = `Total in cart: $${getTotal()}`;
@@ -88,7 +142,7 @@ function getQty() {
     for (let i = 0; i < cart.length; i += 1) {
         qty += cart[i].qty;
     }
-    return;
+    return qty;
 }
 
 // get total
@@ -98,21 +152,6 @@ function getTotal() {
         total += cart[i].price * cart[i].qty;
     }
     return total.toFixed(2);
-}
-
-function removeItem(name, qty = 0) {
-    for (let i = 0; i < cart.length; i += 1) {
-        if (cart[i].name === name) {
-            if (qty > 0) {
-                cart[i].qty -= qty;
-            }
-            cart[i].qty -= 1;
-            if (cart[i].qty < 1 || qty === 0) {
-                cart.splice(i, 1);
-            }
-            return;
-        }
-    }
 }
 
 showItems();
